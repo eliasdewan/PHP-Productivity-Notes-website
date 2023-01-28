a330ab53bafa07581fb50955df0427c7cd62dc90
Elias <k2056101@kingston.ac.uk>
2023-01-28 14:41:52 +0000

    table style to display the projects

--- mainpage.php
+++ mainpage.php
@@ -28,6 +28,12 @@
             color: #000000;
             background-color: #ffffff;
         }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
     </style>
 </head>
 
@@ -44,8 +50,51 @@
 
     <?php foreach ($projects as $project) : ?>
         <p><?= $project->projectid, " ", $project->priority ?> </p>
+    <?php endforeach ?>
+
+
+    <?php foreach ($users as $user) :
+        $userRowSpan = 0;
+        foreach ($user->projects as $project) {
+            $userRowSpan += count($project->sections);
+        }
+    ?>
+        <table>
+            <tr>
+                <th> user name</th>
+                <th> prject title</th>
+                <th> section</th>
+            </tr>
+
+            
+            <?php
+            foreach ($user->projects as $project) :
+                $projectRowSpan = 0;
+                $projectRowSpan += count($project->sections); // for now while there is no task
+
+                //  foreach ($project->sections as $section) { }
+
+                foreach ($project->sections as $section) : ?>
+                    <tr>
+                        <?php if ($section ==  $project->sections[0] and $project ==  $user->projects[0]) : ?>
+                            <td rowspan="<?= $userRowSpan  ?>"><?= $user->username . $userRowSpan  ?></td>
+                        <?php endif ?>
+
+                        <?php if ($section ==  $project->sections[0]) : ?>
+                            <td rowspan="<?= $projectRowSpan  ?>"><?= $project->title . $projectRowSpan  ?></td>
+                        <?php endif ?>
+
+                        <td><?= $section->title ?></td>
+                    </tr>
+
+            <?php
+                endforeach;
+            endforeach ?>
+        </table>
 
     <?php endforeach ?>
+
+
 </body>
 
 </html>
