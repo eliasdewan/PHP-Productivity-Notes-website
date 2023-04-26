36c4152ff0dc9b263ecbee646a0c7a79540e7e9a
Elias <k2056101@kingston.ac.uk>
2023-04-26 15:14:24 +0100

    implemented favicon and web link

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ $(document).ready(function() {
       this.style.height = 'auto';
       this.style.height = this.scrollHeight + 'px';
     });
+
+
+
+    $('a').each(function() {
+        var link = $(this).attr('href');
+        if (link && link.startsWith('http')) {
+          $.ajax({
+            url: link,
+            type: 'GET',
+            success: function(response) {
+              var title = $(response).filter('title').text();
+              $('#website-title').append('<p>' + title + '</p>');
+            }
+          });
+        }
+      });
+
+
+
   });
