7801b4a4f2ed69f146ba48997ef6cc11e766cc3c
Elias Dewan <come1stai2amico3@live.it>
2023-11-30 14:38:48 +0000

    coomenting out tiymce premium

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function addClassEditorButton() {
 function initializeEditor() {
     tinymce.init({
         selector: 'textarea.editor',
-        plugins: ' autoresize anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+        plugins: ' autoresize anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount ',//checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
         autoresize_min_height: 30,
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
         tinycomments_mode: 'embedded',
